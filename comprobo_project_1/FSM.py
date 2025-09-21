462462ffe6378014ca0bed32f7184af80ad71054
Julian Shah <julakshah@gmail.com>
2025-09-21 16:51:55 -0400

    functioning threading

--- comprobo_project_1/FSM.py
+++ comprobo_project_1/FSM.py
@@ -1,63 +1,98 @@
 import rclpy
 from rclpy.node import Node
+from threading import Thread, Event
+# from Time import sleep
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PointStamped, Point
+from std_msgs.msg import Header
+import math
 
 class FSM(Node):
+    """A signel node ros2 program that transforms a neato into the
+    ultimate law keeping device: aka. THE SHERIFF"""
+
     def __init__(self):
-        # Create a timer that fires ten times per second
-        timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.run_loop)
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        # Name the node discoverable
+        super().__init__('sheriff_fsm')
+
         # global variables
-        int state = 0 # the state handler variable
-        int timepost = 0 # the time lookout() state change in ms
-        int buffer = 500 # the time of a lookout() swing in ms
-        float proximity = 1 # the distance of detecting to chase in m
+        self.state = 0 # the state handler variable
+        self.timepost = 0 # the time lookout() state change in ms
+        self.buffer = 500 # the time of a lookout() swing in ms
+        self.proximity = 1 # the distance of detecting to chase in m
+        self.current_time = 0.0;
+
+        # pubs & subs
+        self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.time_pub = self.create_publisher(Header, 'FSM_time', 10) # maybe unnecessary
+        self.create_subscription(LaserScan, 'scan', self.get_scan, 10)
+        self.create_subscription(Header, 'FSM_time', self.get_time, 1)
         
+        # Run threads
+        self.create_timer(0.1, self.run_threads)
+
+    def run_threads(self):
+        self.run_loop_thread = Thread(target=self.run_loop())
+        self.run_time_loop_thread = Thread(target=self.time_loop())
+        self.run_time_loop_thread.start()
+        self.run_loop_thread.start()
+
     def run_loop(self):
         """run the robot states"""
         # check transitions
         self.state_handler()
 
         # run the current state
-        switch(state):
+        match self.state:
             case 0:
-                star()
-                break
+                self.star()
             case 1:
-                chase()
-                break
+                self.chase()
             case 2:
-                lookout()
-                break
+                self.lookout()
         
     def state_handler(self): 
         """Handles transitions between states"""
         # find the closest lidar distance
-        lidar_dist  = #DO THIS
-
-        # find the current time
-        current_time = #DO THIS
+        lidar_dist  = 0 #DO THIS
 
         # transitions between FSM
-        if lidar_dist < proximity:
-            state = 1
-        elif (state == 1 && lidar_dist > proximity):
-            state = 2
-            timepost = #current time goes here
-        elif (state == 2 && current_time >= timepost + (buffer * 3))
-            state = 0
+        if lidar_dist < self.proximity:
+            self.state = 1
+        elif (state == 1 and lidar_dist > self.proximity):
+            self.state = 2
+            self.timepost = self.current_time
+        elif (self.state == 2 and self.current_time >= self.timepost + (self.buffer * 3)):
+            self.state = 0
 
 
     def star(self): 
         # Makes robot move in star
+        print(0)
 
     def chase(self): 
         # Makes robot chase a person
+        print(1)
         
     def lookout(self): 
         # Makes robot go through lookout sequence
+        print(2)
+
+    def time_loop(self):
+        # Keeps track of time since the node begun
+        print("running time loop")
+        time_header = Header(stamp=self.get_clock().now().to_msg(), frame_id="neato_FSM")
+        print(time_header)
+        #self.vel_pub.pubish(time_header)
+        
+    def get_scan(self, msg):
+        filler = 0
+
+    def get_time(self, msg):
+        # Gets the time of time thread
+        print("getting time")
+        self.current_time = 0
+        
 
 def main(args=None):
     rclpy.init(args=args)
